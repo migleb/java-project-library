342eb97b73fdb977898c52bed78907e1f4be8ff7
Migle Bucelyte <migle.bucelyte@outlook.com>
2015-04-19 19:55:20 +0300

    BooksTab created

---
+++ src/BooksTab.java
@@ -0,0 +1,117 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+class BooksTableModel extends DefaultTableModel{
+	
+	public BooksTableModel(String[] columnNames, int i){
+		super(columnNames, i);
+	}
+	
+	@Override
+	public boolean isCellEditable(int row, int column) {                
+		return false;
+	};
+}
+
+public class BooksTab extends Tab {
+	
+	  DefaultTableModel table;
+	  ArrayList<Object[]> booksList;
+	  String currentPath = ".";
+	
+	BooksTab(JPanel tab){
+		
+		lineBox.add(add);
+	    lineBox.add(edit);
+	    tab.add(lineBox, BorderLayout.NORTH);
+	    
+	    // PRIDETI YEAR!!!!!
+	    String[] columnNames = {"ID", "Title", "Author", "Year", "Quantity", "Available"};
+	    this.table = new BooksTableModel(columnNames, 0);
+		JTable dataTable = new JTable(this.table);
+		
+		dataTable.addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				if (e.getClickCount() == 2){
+					JTable target = (JTable)e.getSource();
+					int row = target.getSelectedRow();
+					
+					analyzeRow(row);
+				}
+			}
+		});
+		
+	printFiles();
+	
+	JScrollPane scrollPane = new JScrollPane(dataTable);
+	tab.add(scrollPane);
+	
+}
+
+  public void printFiles(){
+		this.table.setRowCount(0);
+		booksList = new ArrayList<Object[]>();
+		
+		int id;
+		String title;
+		String author;
+		int quantity;
+		int available;
+		int year;
+		
+		try {
+		      Class.forName("org.sqlite.JDBC");
+		      c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		      c.setAutoCommit(false);
+
+		      stmt = c.createStatement();
+		      
+		      ResultSet rs = stmt.executeQuery( "SELECT * FROM LIBRARY;" );
+		      while ( rs.next() ) {
+		         id = rs.getInt("id");
+		         title = rs.getString("title");
+		         author  = rs.getString("author");
+		         quantity = rs.getInt("quantity");
+		         available = rs.getInt("available");
+		         year = rs.getInt("year");
+		         Object[] bookInfo = new Object[]{
+							id,
+							title,
+							author,
+							year,
+							quantity,
+							available
+						};
+		         this.table.addRow(bookInfo);
+		         this.booksList.add(bookInfo);
+		         
+		      }
+		      rs.close();
+	   
+		      stmt.close();
+		      c.close();
+		    } catch ( Exception e ) {
+		    	System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		    	System.exit(0);
+		    }
+		
+		/*try {
+			this.currentPath = folder.getCanonicalPath();
+			System.out.println(this.currentPath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			
+		}*/
+	  }
+	  
+	  public void analyzeRow(int num){
+		  
+	  }
+}
