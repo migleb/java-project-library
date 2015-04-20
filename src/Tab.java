17f91a78ee063afde5290b08365708364a2132b8
Migle Bucelyte <migle.bucelyte@outlook.com>
2015-04-20 10:08:01 +0300

    BooksTableModel added

--- src/Tab.java
+++ src/Tab.java
@@ -4,7 +4,19 @@ import java.sql.Statement;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.table.DefaultTableModel;
 
+class BooksTableModel extends DefaultTableModel{
+	
+	public BooksTableModel(String[] columnNames, int i){
+		super(columnNames, i);
+	}
+	
+	@Override
+	public boolean isCellEditable(int row, int column) {                
+		return false;
+	};
+}
 
 public class Tab {
     Connection c = null;
