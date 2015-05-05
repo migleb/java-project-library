ca9893bee5dba75f83e5f83d7832d525858e3bca
Migle Bucelyte <migle.bucelyte@outlook.com>
2015-05-05 18:31:16 +0300

    new exception classes

---
+++ src/com/mif/mibu1254/views/ProjectException.java
@@ -0,0 +1,12 @@
+package com.mif.mibu1254.views;
+
+public class ProjectException extends Exception{
+	
+	public ProjectException(){
+		
+	}
+	
+	public ProjectException(String message){
+		super(message);
+	}
+}
---
+++ src/com/mif/mibu1254/views/WrongYearException.java
@@ -0,0 +1,8 @@
+package com.mif.mibu1254.views;
+
+
+public class WrongYearException extends ProjectException{
+	public WrongYearException(){		
+		super("Wrong year.");
+	}
+}
